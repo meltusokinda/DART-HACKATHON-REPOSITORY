541f03ec84b482c62106b99eb52e462b6b25568d
Okinda <Okindameltus00@gmail.com>
2024-03-19 00:11:51 +0300

    dart hackathon

---
+++ program1.dart
@@ -0,0 +1,12 @@
+// Program 1: Display Personal Information
+//Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+
+void main () {
+//defining variables
+String name = "Meltus Okinda";
+int age = 24;
+String school = "Multimedia University Of Kenya";
+String hobby = "Watching movies";
+// print a sentence
+  print("My name is  $name, I am $age years old, and  I study at $school.In my free time, I enjoy $hobby ");
+}
