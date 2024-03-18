1189ec2cc45ca13dc8835869d5500ed46641df91
Okinda <Okindameltus00@gmail.com>
2024-03-19 00:12:23 +0300

    dart hackathon

--- controlflow.dart
+++ controlflow.dart
@@ -1,7 +1,26 @@
 
-## Program 3: Determine Grade Based on Marks
-Write a Dart program to determine the grade based on a student's marks. The program should print out the appropriate grade according to the following criteria:
-- If the marks are greater than 85, print "Excellent".
-- If the marks are between 75 and 85 (inclusive), print "Very Good".
-- If the marks are between 65 and 75 (inclusive), print "Good".
-- If the marks are below 65, print "Average".
+// ## Program 3: Determine Grade Based on Marks
+// Write a Dart program to determine the grade based on a student's marks. The program should print out the appropriate grade according to the following criteria:
+// - If the marks are greater than 85, print "Excellent".
+// - If the marks are between 75 and 85 (inclusive), print "Very Good".
+// - If the marks are between 65 and 75 (inclusive), print "Good".
+// - If the marks are below 65, print "Average".
+void main () {
+    print("Enter the student`s marks(out of 100): ");
+    int marks = 80;
+    String grade;
+    if (marks > 85) {
+      grade = "Excellent";
+    }
+    else if (marks > 75) {
+      grade = "Very Good";
+    }
+    else if (marks > 65) {
+      grade = "Good";
+    }
+    else {
+      grade = "average";
+    }
+    // print the grade
+    print("The student`s grade is: $grade");
+}
--- functions.dart
+++ functions.dart
@@ -1,2 +1,16 @@
-## Program 2: Perform Mathematical Operations with Functions
-Write a Dart program that performs two mathematical operations using functions.
+//  Program 2: Perform Mathematical Operations with Functions
+// Write a Dart program that performs two mathematical operations using functions.
+void main() {
+    int number1 = 80;
+    int number2 = 90;
+
+    int add(int a, int b) {
+      return a + b;
+    }
+    int multiply(int a, int b) {
+      return a * b;
+    }
+    int sum = add(number1, number2);
+    int product = multiply(number1, number2);
+    print("The sum of $number1 and $number2 is $sum, and their product is $product.");
+}
